b32b3ecfb70a2e0126bd5844438c5c42c90f3721
agentbonjovi <sasha.listoff@yandex.ru>
2023-10-30 01:12:57 +0300

    Update main.py

--- RK1/main.py
+++ RK1/main.py
@@ -29,7 +29,8 @@ brausers = [
     brau(1, 'safari', 173, 1),
     brau(2, 'chrome', 140,  2),
     brau(3, 'mozilla', 97, 3),
-    brau(4, 'Opera', 201, 4)
+    brau(4, 'Opera', 201, 4),
+    brau(5, 'Edge', 50, 5)
 ]
 
 comps_braus = [
@@ -61,25 +62,30 @@ def main():
                     for b in brausers if b.id == brau_id
                     ]
 
-    print('Задание A1')
-    res_1 = sorted(one_to_many, key=itemgetter(2))
+    print('Задание Д1')
+    res_1 = []
+    for brau in one_to_many:
+        if brau[0][-1] == 'a':
+            res_1.append((brau[0], brau[2]))
     print(res_1)
 
-    print('Задание A2')
+
+    print('Задание Д2')
     res_2_unsorted = []
     for c in computers:
-        c_braus = list(filter(lambda i: i[2]==c.name, one_to_many))
+        c_braus = list(filter(lambda i: i[2]==c.name, many_to_many))
         if len(c_braus) > 0:
             c_sizes = [size for _,size,_ in c_braus]
             c_sizes_sum = sum(c_sizes)
-            res_2_unsorted.append((c.name, c_sizes_sum))
-    res_2 = sorted(res_2_unsorted, key=itemgetter(1), reverse = True)
+            k = len(c_sizes)
+            res_2_unsorted.append((c.name, c_sizes_sum/k))
+    res_2 = sorted(res_2_unsorted, key=itemgetter(1), reverse=True)
     print(res_2)
 
-    print('Задание А3')
+    print('Задание Д3')
     res_3 = {}
     for c in computers:
-        if 'a' in c.name:
+        if 'msi' in c.name:
             c_braus = list(filter(lambda i: i[2]==c.name, many_to_many))
             c_braus_names = [x for x,_,_ in c_braus]
             res_3[c.name] = c_braus_names
